bf4919f7c28316170174bb0d15b3f70bca9aed61
james-keevy <jameskeevy@gmail.com>
2025-04-13 10:19:20 +0200

    Update app

--- ascendra.py
+++ ascendra.py
@@ -2,7 +2,8 @@ import streamlit as st
 import csv
 import pandas as pd
 from collections import defaultdict
-from openai import OpenAI
+from openai 
+import OpenAI
 from datetime import datetime
 import re
 from fpdf import FPDF
